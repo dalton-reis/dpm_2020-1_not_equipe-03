464d7d2e3927170e3ab99280118231ebb8f3d8b7
Leonardo Rovigo <leonardorovigo@gmail.com>
2020-04-22 20:12:51 -0300

    Adicionado pasta da unidade 3 e o Hello World do unity(A6)

---
+++ Unidade 3/A6/Assets/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "Hello World!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
