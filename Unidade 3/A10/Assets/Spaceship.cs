66bdd47020e15b46d318c666667e68ee849eff79
jardelangelo <38145390+jardelangelo@users.noreply.github.com>
2020-04-29 18:49:34 -0300

    Update Spaceship.cs

--- Unidade 3/A10/Assets/Spaceship.cs
+++ Unidade 3/A10/Assets/Spaceship.cs
@@ -7,6 +7,7 @@ public class Spaceship : MonoBehaviour
 
 	Rigidbody2D rb;
 	float dirX;
+	float dirY;
 	float moveSpeed = 20f;
 
 	// Use this for initialization
@@ -19,12 +20,13 @@ public class Spaceship : MonoBehaviour
 	void Update()
 	{
 		dirX = Input.acceleration.x * moveSpeed;
-		transform.position = new Vector2(Mathf.Clamp(transform.position.x, -7.5f, 7.5f), transform.position.y);
+		dirY = Input.acceleration.y * moveSpeed;
+		transform.position = new Vector2(Mathf.Clamp(transform.position.x, -7.5f, 7.5f), Mathf.Clamp(transform.position.y, -4f, 4f));
 	}
 
 	void FixedUpdate()
 	{
-		rb.velocity = new Vector2(dirX, 0f);
+		rb.velocity = new Vector2(dirX, dirY);
 	}
 
 }
