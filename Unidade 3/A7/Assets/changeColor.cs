67996b0b2df840c03081aa68676a4c87c91ef6f4
gabriel.salvador <gabriel.salvador@senior.com.br>
2020-05-13 14:42:41 -0300

    adicionado o A7

---
+++ Unidade 3/A7/Assets/changeColor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        
+    }
+
+    public void setColor(string cor)
+    {
+        RawImage imagem = this.gameObject.GetComponent<RawImage>();
+        switch (cor)
+        {
+            case ("red"):
+                imagem.color = Color.red;
+                break;
+            case ("green"):
+                imagem.color = Color.green;
+                break;
+            case ("white"):
+                imagem.color = Color.white;
+                break;
+            case ("blue"):
+                imagem.color = Color.blue;
+                break;
+        }
+    }
+
+}
